b23f505c83489386655c24a257674f8196a03989
Eleves <eleves@grasset.qc.ca>
2022-07-19 14:13:49 -0400

    Premier commit du code visual studio

---
+++ SDK_Equipe_2_Pat/SDK_Equipe_2_Pat/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SDK_Equipe_2_Pat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
